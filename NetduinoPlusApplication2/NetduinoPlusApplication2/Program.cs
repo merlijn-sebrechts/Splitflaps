c47e5021906c30ae850c3b08d50cdf4f35a96a3a
Merlijn Sebrechts <merlijn.sebrechts@gmail.com>
2013-02-15 16:14:29 +0100

    first

---
+++ NetduinoPlusApplication2/NetduinoPlusApplication2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.NetduinoPlus;
+using NetduinoPlusApplication2;
+
+namespace NetduinoPlusApplication1
+{
+    public class Program
+    {
+
+
+        public static void Main()
+        {
+            //http://support2.microchip.com/KBSearch/KB_StdProb.aspx?ID=SQ6UJ9A00A2VV
+            //http://wiki.netduino.com/SPI.ashx
+            //http://wiki.netduino.com/SPI-Configuration.ashx
+            //
+            //---MCP---
+            //CS   11
+            //SCK  12
+            //SI   13
+            //SO   14
+            //
+            //---netduino---
+            //SCK  D13
+            //MISO D12
+            //MOSI D11
+            //SL   D10
+            //
+            //---flat cable---
+            //GND   1
+            //Vdd   2
+            //SO    3
+            //SI    4
+            //SCK   5
+            //CS    6
+            //
+            //---MCP -> output ---
+            //B0     8
+            //B1     7
+            //B2     6
+            //B3     5
+            //B4     4
+            //B5     3
+            //B6     2
+            //B7     1    
+            //A0     9   
+            //A1    10
+            //A2    11
+            //A3    12
+            //A4    13
+            //A5    14
+            //A6    15
+            //A7    16
+
+
+
+
+
+            SplitflapDriver driver = new SplitflapDriver();
+            driver.Test();
+
+
+        }
+    }
+}
--- NetduinoPlusApplication2/NetduinoPlusApplication2/SplitflapDriver.cs
+++ NetduinoPlusApplication2/NetduinoPlusApplication2/SplitflapDriver.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
@@ -6,10 +6,11 @@ using Microsoft.SPOT;
 using Microsoft.SPOT.Hardware;
 using SecretLabs.NETMF.Hardware;
 using SecretLabs.NETMF.Hardware.NetduinoPlus;
+using NetduinoPlusApplication2;
 
-namespace NetduinoPlusApplication1
+namespace NetduinoPlusApplication2
 {
-    public class Program
+    class SplitflapDriver
     {
         public enum Registers
         {
@@ -37,11 +38,49 @@ namespace NetduinoPlusApplication1
             OLATB
         };
 
-        public static void Main()
+        public void Test()
         {
             //http://support2.microchip.com/KBSearch/KB_StdProb.aspx?ID=SQ6UJ9A00A2VV
             //http://wiki.netduino.com/SPI.ashx
-            //http://wiki.netduino.com/SPI-Configuration.ashx   
+            //http://wiki.netduino.com/SPI-Configuration.ashx
+            //
+            //---MCP---
+            //CS   11
+            //SCK  12
+            //SI   13
+            //SO   14
+            //
+            //---netduino---
+            //SCK  D13
+            //MISO D12
+            //MOSI D11
+            //SL   D10
+            //
+            //---flat cable---
+            //GND   1
+            //Vdd   2
+            //SO    3
+            //SI    4
+            //SCK   5
+            //CS    6
+            //
+            //---MCP -> output ---
+            //B0     8
+            //B1     7
+            //B2     6
+            //B3     5
+            //B4     4
+            //B5     3
+            //B6     2
+            //B7     1    
+            //A0     9   
+            //A1    10
+            //A2    11
+            //A3    12
+            //A4    13
+            //A5    14
+            //A6    15
+            //A7    16
 
             OutputPort led = new OutputPort(Pins.ONBOARD_LED, false);
 
@@ -58,14 +97,18 @@ namespace NetduinoPlusApplication1
 
             while (true)
             {
-                spi.Write(new byte[] { 0x42, 0x12, 0xff });   // Set bank A outputs to high
-                spi.Write(new byte[] { 0x42, 0x13, 0xff });   // Set bank B outputs to high
+                spi.Write(new byte[] { 0x42, 0x12, 0x55 });   // Set bank A outputs to high
+                spi.Write(new byte[] { 0x42, 0x13, 0xAA });   // Set bank B outputs to high
 
 
                 led.Write(true); // turn on the LED 
-                Thread.Sleep(1500); // sleep for 250ms 
+                Thread.Sleep(500); // sleep for 250ms 
+
+                spi.Write(new byte[] { 0x42, 0x12, 0xAA });   // Set bank A outputs to high
+                spi.Write(new byte[] { 0x42, 0x13, 0x55 });   // Set bank B outputs to high
+
                 led.Write(false); // turn off the LED 
-                Thread.Sleep(1500); // sleep for 250ms 
+                Thread.Sleep(500); // sleep for 250ms 
             }
         }
     }
